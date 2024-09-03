2962840e64efbe10badedf0dcc754a86e3463a4f
lilyyllyyllyly <lilylylith@gmail.com>
2024-09-03 16:28:18 -0300

    Remove most fake changes when saving scenes
    Previously, saving the level scene would always add changes due to the
    flying fish's animations and the end boat WaveMover behaviour. The
    flying fishes now have a RESET track that is reverted to when saving,
    and the WaveMover script has a variable to control wether it should run
    in the editor, to avoid these issues. However, the tilemap still creates
    fake changes when closing and reopening the scene, with signal
    connections being remade. For now I see no way to fix that.

--- Scripts/Behaviours/WaveMover.gd
+++ Scripts/Behaviours/WaveMover.gd
@@ -1,6 +1,8 @@
 @tool
 class_name WaveMover extends Behaviour
 
+@export var run_on_editor: bool
+
 @export var obj: Node2D
 
 @export var amplitude: float
@@ -9,5 +11,6 @@ class_name WaveMover extends Behaviour
 @export var speed: float
 
 func _process(delta):
+	if Engine.is_editor_hint() and !run_on_editor: return
 	obj.position.y = (sin(obj.global_position.x * frequency + Time.get_ticks_msec() / 1000.0 * speed + offset) + 1.0) / 2.0 * amplitude;
 
